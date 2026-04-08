c18925482c5a964feac47376714d681bc4a04c89
Geri Adam Saputra <geriadam04@gmail.com>
2026-04-08 20:35:57 +0000

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 // .eleventy.js
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("dist/styles");
+    eleventyConfig.addPassthroughCopy("dist/js");
     eleventyConfig.addPassthroughCopy({
         "src/assets/img": "/dist/assets/img/",
     });
